f2f13992e55e9bf36fa66cf7ccc67c6fc1fb6081
RomanBurlakov <roma160398@yandex.ru>
2025-01-17 00:03:11 +0300

    add arrayMethods

---
+++ arrayMethods.js
@@ -0,0 +1,23 @@
+function mySlice(arr, start = 0, end = arr.length) {
+	const newArray = [];
+	if (end < 0) end = arr.length + end;
+	if (start < 0) start = arr.length + start;
+	for (let i = start; i < end; i++) {
+		newArray.push(arr[i]);
+	}
+	return newArray;
+}
+
+function myIndexOf(arr, item, from = 0) {
+	for (let i = from; i < arr.length; i++) {
+		if (arr[i] === item) return i;
+	}
+	return -1;
+}
+
+function myIncludes(arr, item, from = 0) {
+	for (let i = from; i < arr.length; i++) {
+		if (arr[i] === item) return true;
+	}
+	return false;
+}
